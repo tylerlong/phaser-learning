e8aebf06eeb93f1ccd1435571d8e632b8bcc16c0
Tyler Long <tyler4long@gmail.com>
2020-05-24 13:40:00 -0700

    Build output to docs folder

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,9 +1,13 @@
 /* eslint-disable node/no-unpublished-import */
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import webpack from 'webpack';
+import path from 'path';
 
 const config: webpack.Configuration = {
   entry: ['./src/index.ts'],
+  output: {
+    path: path.resolve(__dirname, 'docs'),
+  },
   devtool: 'source-map',
   plugins: [new HtmlWebpackPlugin({title: 'Phaser Learning'})],
   module: {
